37ba8f54187f7beb0d7b40a9ec90e8d8a6011149
poseidonsanket <sanketdadali05@gmail.com>
2024-03-04 18:20:11 +0530

    Actual project Setup done

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,18 @@
-import { Fragment } from "react";
-import Link from "next/link";
+import MeetupList from "../components/meetups/MeetupList";
 
+const HomePage = () => {
+    const Dummy_MeetUp = [
+        {
+            id: "m1",
+            title: "A first meetup",
+            image: "https://imgs.search.brave.com/NtmEfPH0tfwV0IMfXhG3IXnhiF-KjIK9zWAo1BmRFI8/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAxMy8w/NC8xMS8xOS80Ni9i/dWlsZGluZy0xMDI4/NDBfNjQwLmpwZw",
+            address: "xyx",
+            description: "xyz"
+        }
+    ]
+    return <div>
+        <MeetupList meetups={Dummy_MeetUp}/>
+    </div>
+}
 
-const Home = () => {
-  return (
-    <Fragment>
-      <h1 className="bold">HomePage</h1>
-      <ul>
-        <li className="decoration-inherit">
-          <Link href="/news/NextJs">Next JS is Great FrameWork</Link>
-        </li>
-        <li>Something Else</li>
-      </ul>
-    </Fragment>
-  );
-};
-export default Home;
+export default HomePage;
--- pages/news/index.js
+++
@@ -1,7 +0,0 @@
-
-
-const NewsPage = () => {
-  return <h1>NewsPage</h1>;
-};
-
-export default NewsPage;
