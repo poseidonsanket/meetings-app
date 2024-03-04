644ad1bfc6746750c1989b5b8317f1e6d69fc414
poseidonsanket <sanketdadali05@gmail.com>
2024-03-04 18:00:18 +0530

    Link Added

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,18 @@
-import NewsPage from "./news";
+import { Fragment } from "react";
+import Link from "next/link";
+
 
 const Home = () => {
-  return <div>HomePage</div>;
+  return (
+    <Fragment>
+      <h1 className="bold">HomePage</h1>
+      <ul>
+        <li className="decoration-inherit">
+          <Link href="/news/NextJs">Next JS is Great FrameWork</Link>
+        </li>
+        <li>Something Else</li>
+      </ul>
+    </Fragment>
+  );
 };
 export default Home;
