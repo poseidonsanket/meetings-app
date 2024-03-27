9934ecfcabeecd45e65e5e30898719ce0881e4a3
poseidonsanket <sanketdadali05@gmail.com>
2024-03-27 17:57:34 +0530

    Navbar Added

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,21 @@
 import MeetupList from "../components/meetups/MeetupList";
 
 const HomePage = () => {
-    const Dummy_MeetUp = [
-        {
-            id: "m1",
-            title: "A first meetup",
-            image: "https://imgs.search.brave.com/NtmEfPH0tfwV0IMfXhG3IXnhiF-KjIK9zWAo1BmRFI8/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAxMy8w/NC8xMS8xOS80Ni9i/dWlsZGluZy0xMDI4/NDBfNjQwLmpwZw",
-            address: "xyx",
-            description: "xyz"
-        }
-    ]
-    return <div>
-        <MeetupList meetups={Dummy_MeetUp}/>
+  const Dummy_MeetUp = [
+    {
+      id: "m1",
+      title: "A first meetup",
+      image:
+        "https://imgs.search.brave.com/NtmEfPH0tfwV0IMfXhG3IXnhiF-KjIK9zWAo1BmRFI8/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAxMy8w/NC8xMS8xOS80Ni9i/dWlsZGluZy0xMDI4/NDBfNjQwLmpwZw",
+      address: "xyx",
+      description: "xyz",
+    },
+  ];
+  return (
+    <div>
+      <MeetupList meetups={Dummy_MeetUp} />
     </div>
-}
+  );
+};
 
 export default HomePage;
