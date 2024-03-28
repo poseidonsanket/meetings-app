85c8cd3c28c41c911c6876ea92a2f91232f41004
poseidonsanket <sanketdadali05@gmail.com>
2024-03-28 17:24:05 +0530

    Api putting data to mongodb added

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,13 +1,28 @@
-import NewMeetupForm from "../../components/meetups/NewMeetupForm";
-const NewMeetupPage = () => {
-  const addMeetupHandler = () => {
-    console.log("new Meetup");
-  };
-  return (
-    <div>
-      <NewMeetupForm onAddMeetup={addMeetupHandler} />
-    </div>
-  );
-};
+// our-domain.com/new-meetup
+import { useRouter } from 'next/router';
+
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+
+function NewMeetupPage() {
+  const router = useRouter();
+
+  async function addMeetupHandler(enteredMeetupData) {
+    const response = await fetch('/api/new-meetup', {
+      method: 'POST',
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    router.push('/');
+  }
+
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />
+}
 
 export default NewMeetupPage;
